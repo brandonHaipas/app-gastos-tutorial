70f835ac5514de8b4e2b2bea449ea848a98cd92b
Brandon Peña <brandon.pena96@gmail.com>
2024-07-03 01:51:41 -0400

    now you can search for date range and delete expenses

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os.path
-import utils
+from utils import *
 path = 'gastos.csv'
 
 if not os.path.isfile(path):
@@ -20,12 +20,37 @@ while True:
                    3.- buscar gastos por categoria
                    4.- ver las estadisticas totales de los gastos
                    5.- actualizar un gasto
-                   6.- eliminar un gasto\n""")
-    opcion = input('operacion a realizar')
+                   6.- eliminar un gasto
+                   7.- terminar el programa\n""")
+    opcion = input('operacion a realizar: ')
     if opcion == '1':
-        pass
+        print("Ingrese los datos del registro a continuacion: \n")
+        descripcion = input('Breve descripcion:\n')
+        cat = input("categoria:\n")
+        dia = input('dia:\n')
+        mes = input('mes:\n')
+        anho = input('año:\n')
+        costo = input('costo:\n')
+        
+        fecha = '-'.join([dia,mes,anho])#dia-mes-anho
+        ingresar_gasto(descripcion, cat , fecha , costo, 'gastos.csv')
+        
     elif opcion == '2':
-        pass
+        print("A continuacion ingrese la fecha de inicio y la fecha de termino del periodo de gastos que quiere ver: \n")
+        print('Fecha de inicio\n')
+        dia_inicio = input('dia\n')
+        mes_inicio = input('mes\n')
+        anho_inicio = input('año\n')
+        print('Fecha de termino\n')
+        dia_termino = input('dia\n')
+        mes_termino = input('mes\n')
+        anho_termino = input('año\n')
+        
+        start = '-'.join([dia_inicio, mes_inicio, anho_inicio])
+        stop = '-'.join([dia_termino, mes_termino, anho_termino])
+        filtrados = buscar_por_fechas('gastos.csv', start, stop)
+        for gasto in filtrados:
+            print(gasto_a_string(gasto) + '\n')
     elif opcion == '3':
         pass
     elif opcion == '4':
@@ -33,4 +58,7 @@ while True:
     elif opcion == '5':
         pass
     elif opcion == '6':
+        index = input("indice del gasto a eliminar: ")
+        eliminar_gasto(index, 'gastos.csv')
+    elif opcion == '7':
         break
--- utils.py
+++ utils.py
@@ -1,9 +1,15 @@
 from datetime import datetime
 def ingresar_gasto(descripcion, cat, fecha, monto, file):
     #handling usuario
+    index = '0'
+    gastos_lista = crear_lista_gastos(file)
+    len_gasto = len(gastos_lista)
+    if len_gasto>0:
+        index = str(int(gastos_lista[len_gasto-1]['index'])+1)
+        
     try:
         with open(file, 'a') as gastos:
-            line = descripcion +'\t' + cat +'\t'+ fecha + '\t'+ monto + '\n'
+            line = index + '\t' + descripcion +'\t' + cat +'\t'+ fecha + '\t'+ monto + '\n'
             gastos.write(line)
             print('texto guardado!')
     except Exception as e:
@@ -13,15 +19,19 @@ def crear_lista_gastos(file):
     
     ret_value = []
     try:
-        with open('gastos.csv', 'r') as gastos:
+        with open(file, 'r') as gastos:
             output = gastos.read()
             lines = output.split(sep='\n')
             gastos_list = []
             for line in lines:
+                print("linea",line)
                 if line == '':
                     break
+                
                 line_list = line.split('\t')
-                line_dict = {'descripcion':line_list[0], 'cat':line_list[1], 'fecha':datetime.strptime(line_list[2]), 'costo':line_list[3]}
+                
+                line_dict = {'index':line_list[0], 'descripcion':line_list[1], 'cat':line_list[2], 'fecha':datetime.strptime(line_list[3], '%d-%m-%Y'), 'costo':line_list[4]}
+
                 gastos_list.append(line_dict)
             ret_value = gastos_list
             
@@ -30,7 +40,47 @@ def crear_lista_gastos(file):
     return ret_value
             
 def buscar_por_fechas(file, fecha_inicio, fecha_final):
+    gastos_filtrados = []
     lista_gastos = crear_lista_gastos(file)
     sorted_list = sorted(lista_gastos, key=lambda d: d['fecha'])
-    start = datetime.strptime(fecha_inicio)
-    stop = datetime.strptime(fecha_final)
+    start = datetime.strptime(fecha_inicio, '%d-%m-%Y')
+    stop = datetime.strptime(fecha_final, '%d-%m-%Y')
+    if stop < start:
+        print('no existe este rango de fechas!')
+        
+    for i in sorted_list:
+        if i['fecha'] >= start and  i['fecha']<=stop:
+            gastos_filtrados.append(i)
+        elif i['fecha']> stop:
+            break
+    return gastos_filtrados
+        
+def buscar_por_categoria(file, cat):
+    lista_gastos = crear_lista_gastos(file)
+    return [gasto for gasto in lista_gastos if gasto['cat'] == cat]
+    
+def gasto_a_string(gasto):
+    llaves = gasto.keys()
+    gasto_str = ''
+    for i in llaves:
+        if i == 'fecha':
+            gasto_str += gasto[i].strftime('%d-%m-%Y') + '\t'
+        else:
+            gasto_str += (gasto[i] + '\t')
+    return gasto_str 
+
+def eliminar_gasto(index, file):
+    num_index = int(index)
+    lista_gastos = crear_lista_gastos(file)
+    lista_gastos.pop(num_index)
+    len_gastos = len(lista_gastos)
+    for i in range(num_index, len_gastos):
+        lista_gastos[i]['index'] = str(int(lista_gastos[i]['index']) - 1)
+    try:
+        with open(file, 'w') as gastos_file:
+           gastos_string =  ""
+           for elem in lista_gastos:
+               gastos_string+= gasto_a_string(elem) + '\n'
+           gastos_file.write(gastos_string)
+    except Exception as e:
+        print(f"Error {e}") 
